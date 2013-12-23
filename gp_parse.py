2b6040c971a3aa260be51d388a61517e29dcd8c7
Arash Keshmirian <arash@limbic.com>
2013-12-22 23:34:45 -0800

    first commit

---
+++ gp_parse.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import csv
+import sys
+
+if len(sys.argv) < 2:
+    print "Usage: ./gp_parse.py <filename>"
+    
+
+sourcefile = sys.argv[1]
+
+print "Reading from", sourcefile, "..."
+
+""" read the source file """
+with open(sourcefile, 'rb') as csvfile:
+    gp_reader = csv.reader(csvfile)
+    linenum = 0
+    totals = {}
+    header = ""
+    
+    for row in gp_reader:
+        if linenum == 0:
+            header = row
+        if row[7] == "com.limbic.ac130":
+            if row[14] in totals:
+                totals[row[14]] = float(totals[row[14]])+float(row[15])
+            else:
+                totals[row[14]] = float(row[15])
+            # if row[14] in totals:
+            #     totals[]
+            # if row[14]=="USD":
+                # total += float(row[15])
+        linenum += 1;
+    # print header
+    print totals
