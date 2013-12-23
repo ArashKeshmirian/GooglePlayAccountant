2767be1f71212208b4ecb36354a4e0f78a8e7e9d
Arash Keshmirian <arash@limbic.com>
2013-12-23 01:20:57 -0800

    added best-practices edits

--- gp_parse.py
+++ gp_parse.py
@@ -1,43 +1,54 @@
 #!/usr/bin/python
 import csv
 import sys
+import os
 
-if len(sys.argv) < 2:
-    print "Usage: ./gp_parse.py <filename>"
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: ./gp_parse.py <filename>")
+        return
     
+    kProductIndex = 7
+    kMerchantCurrencyNameIndex = 17
+    kMerchantCurrencyValueIndex = 18
 
-sourcefile = sys.argv[1]
+    sourcefile = sys.argv[1]
 
-print "Reading from", sourcefile, "..."
+    """ read the source file """
+    with open(sourcefile, 'rb') as csvfile:
+        gp_reader = csv.reader(csvfile)
+        linenum = 0
+        products = {}
+        header = ""
 
-""" read the source file """
-with open(sourcefile, 'rb') as csvfile:
-    gp_reader = csv.reader(csvfile)
-    linenum = 0
-    products = {}
-    header = ""
-
-    for row in gp_reader:
-        if linenum == 0:
-            header = row
-        elif row[7] in products:
-            if row[17] in products[row[7]]:
-                product = products[row[7]]
-                product[row[17]] = float(product[row[17]]) + float(row[18])
+        for row in gp_reader:
+            if linenum == 0:
+                header = row
+            elif row[kProductIndex] in products:
+                if row[kMerchantCurrencyNameIndex] in products[row[kProductIndex]]:
+                    product = products[row[kProductIndex]]
+                    product[row[kMerchantCurrencyNameIndex]] = (float(product[row[kMerchantCurrencyNameIndex]]) + 
+                                                                float(row[kMerchantCurrencyValueIndex]))
+                else:
+                    product = products[row[kProductIndex]]
+                    product[row[kMerchantCurrencyNameIndex]] = float(row[kMerchantCurrencyValueIndex])
             else:
-                product = products[row[7]]
-                product[row[17]] = float(row[18])
-        else:
-            products[row[7]] = {row[17]:row[18]}
-        linenum += 1;
-    for product in products:
-        for currency in products[product]:
-            print product, currency, products[product][currency]
+                products[row[kProductIndex]] = {row[kMerchantCurrencyNameIndex]:row[kMerchantCurrencyValueIndex]}
+            linenum += 1;
         
-    # code used to print header-row information for CSV
-    # index = 0
-    # print "\n"
-    # for column in header:
-    #     print index, column
-    #     index += 1
-    
+        # output
+        print("\nFor file: %s"%(os.path.basename(sourcefile)))
+        for product in products:
+            for currency in products[product]:
+                print("\tProduct: %s\n\t\t%s %s"%(product, currency, products[product][currency]))
+        
+        # code used to print header-row information for CSV
+        # index = 0
+        # print "\n"
+        # for column in header:
+        #     print index, column
+        #     index += 1
+
+if __name__ == "__main__":
+  main()
